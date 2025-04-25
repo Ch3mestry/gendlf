644063eb64a4e377e193ee7f42f8ba49da9c92f8
Nikita <corasks@yandex.ru>
2025-04-25 12:31:06 +0300

    public rep 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gendlf/', // Укажите путь к вашему репозиторию
   plugins: [react()],
 })
